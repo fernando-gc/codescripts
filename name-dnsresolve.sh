088a300f593a227e43ebd51d8183d6f3d2f4d762
Laboratorio Digital <40404092+fernando-gc@users.noreply.github.com>
2018-11-28 14:46:56 +0100

    Create name-dnsresolve.sh

---
+++ name-dnsresolve.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+for hostnames in `cat ./hostnames.txt`
+do
+        printf "$hostnames\t"
+        LOOKUP_RES=`nslookup $hostnames`
+        FAIL_COUNT=`echo $LOOKUP_RES | grep ';; connection timed out; ' | wc -l`;
+        if [ $FAIL_COUNT -eq 1 ]
+        then
+             NAME='Bad FQDNS\n';
+        else
+             IP=`echo $LOOKUP_RES | cut -f 5 -d ":" | sed 's/ //'`;
+        fi
+        echo $IP
+done
