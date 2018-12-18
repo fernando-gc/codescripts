b2a1b6c57ac4a05e746dfb988b1fac5237260552
Laboratorio Digital <40404092+fernando-gc@users.noreply.github.com>
2018-12-18 16:31:43 +0100

    Update name-dnsresolve.sh
    Check hostname with IP

--- name-dnsresolve.sh
+++ name-dnsresolve.sh
@@ -5,27 +5,16 @@ do
 
         LOOKUP_RES=`nslookup -timeout=1 $hostnames`
 
-        FAIL_COUNT=`echo $LOOKUP_RES | grep 'NXDOMAIN' | wc -l`;
+        FAIL_COUNT=`echo $LOOKUP_RES | grep 'connection timed out' | wc -l`;
 
         if [ $FAIL_COUNT -eq 1 ]
         then
              IP='Bad-FQDNS\n';
 
         else
-
-        FAIL_COUNT=`echo $LOOKUP_RES | grep 'No answer' | wc -l`;
-
-        if [ $FAIL_COUNT -eq 1 ]
-        then
-             IP='Bad-FQDNS\n';
-
-        else
-
              IP=`echo "$LOOKUP_RES" | grep -e "Address" | tail -n 1 | cut -f 2 -d ":" | sed 's/ //'`;
-
         fi
-fi
+
         echo $IP
 
 done
-
