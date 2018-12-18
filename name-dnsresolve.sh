41c4537112d8e47b09d1a94eebd1346c8225ae2a
Laboratorio Digital <40404092+fernando-gc@users.noreply.github.com>
2018-12-18 16:24:32 +0100

    Update name-dnsresolve.sh

--- name-dnsresolve.sh
+++ name-dnsresolve.sh
@@ -2,13 +2,30 @@
 for hostnames in `cat ./hostnames.txt`
 do
         printf "$hostnames\t"
-        LOOKUP_RES=`nslookup $hostnames`
-        FAIL_COUNT=`echo $LOOKUP_RES | grep ';; connection timed out; ' | wc -l`;
+
+        LOOKUP_RES=`nslookup -timeout=1 $hostnames`
+
+        FAIL_COUNT=`echo $LOOKUP_RES | grep 'NXDOMAIN' | wc -l`;
+
         if [ $FAIL_COUNT -eq 1 ]
         then
-             IP='Bad FQDNS\n';
+             IP='Bad-FQDNS\n';
+
         else
-             IP=`echo $LOOKUP_RES | cut -f 5 -d ":" | sed 's/ //'`;
+
+        FAIL_COUNT=`echo $LOOKUP_RES | grep 'No answer' | wc -l`;
+
+        if [ $FAIL_COUNT -eq 1 ]
+        then
+             IP='Bad-FQDNS\n';
+
+        else
+
+             IP=`echo "$LOOKUP_RES" | grep -e "Address" | tail -n 1 | cut -f 2 -d ":" | sed 's/ //'`;
+
         fi
+fi
         echo $IP
+
 done
+
