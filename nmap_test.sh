f4a5a0dda401446277e9021cbbe51c878948bae0
Laboratorio Digital <40404092+fernando-gc@users.noreply.github.com>
2019-02-28 11:22:53 +0100

    Create nmap test bash
    Add file

---
+++ nmap_test.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+for IP in `cat ./nmap-ip.txt`
+do
+        printf "$IP\t"
+
+        LOOKUP_RES=`nmap -p 6556 -Pn $IP`
+
+        FAIL_COUNT=`echo $LOOKUP_RES | grep "0 hosts up" | wc -l`;
+        if [ $FAIL_COUNT -eq 1 ]
+        then
+             NAME='Host is down';
+
+        else
+             TEST1=`echo $LOOKUP_RES | grep open | wc -l`;
+        if [ $TEST1 -eq 1 ]
+        then
+             NAME='open'
+
+        else
+             TEST2=`echo $LOOKUP_RES | grep filtered | wc -l`;
+        if [ $TEST2 -eq 1 ]
+        then
+             NAME='filtered'
+
+        else
+             TEST3=`echo $LOOKUP_RES | grep closed | wc -l`;
+        if [ $TEST3 -eq 1 ]
+        then
+             NAME='closed'
+
+
+        fi
+        fi
+        fi
+        fi
+        echo $NAME
+
+done
